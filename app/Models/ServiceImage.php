19d01a400256b17ac9a8fa155533fe3e2e3ed1af
devil067 <evgeniy.webdesign@gmail.com>
2022-04-21 08:08:46 +0400

    Repair imag optimizer

--- app/Models/ServiceImage.php
+++ app/Models/ServiceImage.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use File;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ServiceImage extends Model
 {
@@ -26,14 +28,29 @@ class ServiceImage extends Model
         if ($ext == 'jpeg') $ext = 'jpg';
         if (!in_array($ext, ['jpg', 'png', 'gif'])) return;
 
-        $filename = 'service-' . $service->id . '_' . Str::random(3) . '.' . $ext;
+        $filename = 'service-' . $service->id . '_' . Str::random(3);
         $path = 'services/id_' . $service->id;
-        $image->storeAs($path . '/', $filename, 'uploads');
+        $fullPathImage = 'uploads/' . $path . '/' . $filename . '.' . $ext;
+        $fullPathThumb = 'uploads/' . $path . '/' . $filename . '_thumb.' . $ext;
+
+        if (!File::exists('uploads/' . $path)) {
+            File::makeDirectory('uploads/' . $path);
+        }
+
+        $compressImage = Image::make($image);
+        // Save Full Size Image
+        $compressImage->resize(null, 1200, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($fullPathImage, 80);
+        // Save Thumbnail
+        $compressImage->resize(null, 360, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($fullPathThumb, 100);
 
         $newImage = new static();
         $newImage->service_id = $service->id;
-        $newImage->title = $filename;
-        $newImage->image_path = $path . '/' . $filename;
+        $newImage->title = $filename . '.' . $ext;
+        $newImage->image_path = $path . '/' . $filename . '_thumb.' . $ext;
         $newImage->save();
     }
 
@@ -44,9 +61,29 @@ class ServiceImage extends Model
         return false;
     }
 
+    public function getFullImage()
+    {
+        if ($this->image_path != null) {
+            $tmp = explode('.', $this->image_path);
+            $ext = end($tmp);
+            $filename = $tmp[array_key_last($tmp)-1];
+            $newFilename = str_replace('_thumb', '', $filename);
+
+            return '/uploads/' . $newFilename . '.' . $ext;
+        }
+        return false;
+    }
+
     public function removeImage()
     {
         if ($this->image_path != null) {
+
+            $fullImage = $this->getFullImage();
+            if ($fullImage) {
+                $fullImage = str_replace('/uploads/', '', $fullImage);
+                Storage::disk('uploads')->delete($fullImage);
+            }
+
             Storage::disk('uploads')->delete($this->image_path);
             $this->delete();
         }
