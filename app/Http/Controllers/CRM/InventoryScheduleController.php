36427720a51f683cdadcde0d7884e70973e182ed
evgeniy <evgeniy.webdesign@yandex.ru>
2023-03-10 11:00:24 +0400

    СИЗы - количество лет для СИЗов - новая миграция

--- app/Http/Controllers/CRM/InventoryScheduleController.php
+++ app/Http/Controllers/CRM/InventoryScheduleController.php
@@ -53,7 +53,7 @@ class InventoryScheduleController extends Controller
     {
         $request->validate([
             'rate_per_year' => ["required"],
-            'period' => ["required", "numeric"],
+            'period' => ["required"],
         ]);
 
         $schedule->update([
